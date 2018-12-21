09c981918a421b835c3778c299df45f4fe6bff09
Samuel Mussie <punisami@gmail.com>
2018-12-21 18:04:46 +0300

    Added central route file and added componets for posts and comments

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import {Redirect, Route, Switch} from 'react-router-dom';
+
 import Header from "./Header"
 import Footer from "./Footer"
+import routes from "../routes"
 
 export default class Container extends Component {
   render() {
     return (
       <div>
           <Header />
-         Conatinereing INNNNN
+            <Switch>
+            {routes.map((route, idx) => {
+                    return route.component ? <Route key={idx} path={route.path} exact={route.exact} name={route.name} render={props => (
+                        <route.component {...props} />
+                      )} />
+                      : (null);
+                  },
+                )}
+            </Switch>
          <Footer />
       </div>
     )
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Loadable from 'react-loadable'
+import Container from './containers';
+
+function Loading() {
+    return <div>Loading...</div>;
+}
+
+const Posts = Loadable({
+  loader: () => import('./views/post/Posts'),
+  loading: Loading,
+});
+
+const PostView = Loadable({
+    loader: () => import('./views/post/PostView'),
+    loading: Loading,
+  });
+  
+
+// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
+const routes = [
+  { path: '/', exact: true, name: 'Home', component: Posts },
+  { path: '/:topic', exact: true, name: 'Home', component: Posts },
+  { path: '/:topic/:id', exact: true, name: 'Home', component: PostView },
+  
+
+]
+export default routes;
