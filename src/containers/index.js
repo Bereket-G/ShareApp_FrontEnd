5cf12ec56ab7f05ab31336b9de65e341104958d7
Samuel Mussie <punisami@gmail.com>
2018-12-21 17:27:30 +0300

    Added Headre and footer pages

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Header from "./Header"
+import Footer from "./Footer"
 
 export default class Container extends Component {
   render() {
     return (
       <div>
+          <Header />
          Conatinereing INNNNN
+         <Footer />
       </div>
     )
   }
