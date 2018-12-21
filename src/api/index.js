88d0dad89ba33596256d0a516db01fa76b31f84c
Samuel Mussie <punisami@gmail.com>
2018-12-21 21:12:44 +0300

    added signup page and logout service

--- src/api/index.js
+++ src/api/index.js
@@ -8,10 +8,10 @@ export default class Api{
   static API_BASE_URL = API_BASE_URL;
 
   static login = (email, password) => {
-    const url ='users/login?include=roles';
+    const url ='users/login';
     if (email && password) {
         const regExp = new RegExp
-        (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
         return Api.create(url, {[regExp.test(email) ? "email": "username"]: email, "password": password, "ttl": 300000000})
             .then(
                 response => {
@@ -24,7 +24,7 @@ export default class Api{
                 },
                 error => {
                     if (error.response) {
-                        if (error.response.status == 401) {
+                        if (error.response.status === 401) {
                             return {
                                 error: true,
                                 message: "Incorrect username or password"
@@ -45,6 +45,38 @@ export default class Api{
     }
 
   };
+
+  static logout = () => {
+      ClientSession.getAccessToken(function (isLoggedIn, authData) {
+          if (isLoggedIn && authData != null) {
+              Api.create('users/logout', {})
+                  .then(response => {
+                      ClientSession.removeAuth(err => {
+
+                      });
+                      window.location = "#/login";
+                  }).catch( err => {
+                      ClientSession.removeAuth(err => {
+
+                      });
+                      window.location = "#/login";
+                  });
+          }
+      });
+  };
+
+  static signup = (data) => {
+    if(data.username && data.email){
+      return Api.create('users', data)
+        .then( response => {
+          return response;
+        })
+        .catch( error => {
+          return error;
+        })
+    }
+  }
+
   static create(pluralName, data, filter=null){
 
     let url = API_BASE_URL + pluralName;
--- src/views/account/Signup.js
+++ src/views/account/Signup.js
@@ -1,10 +1,44 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Api from '../../api';
 
 export default class Signup extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username:"",
+            password:"",
+            firstname:"",
+            lastname:"",
+            email:""
+        }
+    }
+
+    signup = () => {
+      let data = {
+        email: this.state.email,
+        username: this.state.username,
+        password: this.state.password,
+        firstname: this.state.firstname,
+        lastname: this.state.lastname
+      }
+      Api.signup(data)
+          .then(response => {
+              console.log(response);
+          })
+          .catch(error => {
+              console.log(error);
+          })
+    }
   render() {
     return (
       <div>
-         Signuping  
+        <input type="text" name="firstname" onChange={(e)=>{this.setState({firstname:e.target.value});}}></input> <br />
+        <input type="text" name="lastname" onChange={(e)=>{this.setState({lastname:e.target.value});}}></input> <br />
+        <input type="email" name="email" onChange={(e)=>{this.setState({email:e.target.value});}}></input> <br />
+        <input type="text" name="username" onChange={(e)=>{this.setState({username:e.target.value});}}></input> <br />
+        <input type="password" name="username"  onChange={(e)=>{this.setState({password:e.target.value});}}></input> <br />
+        <button onClick={this.signup}>Sign Up</button>
+        
       </div>
     )
   }
