e36222460a4c30d758c584d5c54cc451afa4d52d
Samuel Mussie <punisami@gmail.com>
2018-12-23 04:01:59 +0300

    built for heroku

---
+++ build/precache-manifest.2d8e31b4cdba36f3b6e898212b0e630f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "afe2806c5e5c3c215fd4",
+    "url": "/static/css/4.d9cf6074.chunk.css"
+  },
+  {
+    "revision": "e79b9eeaef8475759669",
+    "url": "/static/js/0.e79b9eea.chunk.js"
+  },
+  {
+    "revision": "d200a5e6e4edbf8db02e",
+    "url": "/static/js/main.d200a5e6.chunk.js"
+  },
+  {
+    "revision": "cccc68753ca1505514dd",
+    "url": "/static/js/2.cccc6875.chunk.js"
+  },
+  {
+    "revision": "34b79388eefcb47db884",
+    "url": "/static/js/3.34b79388.chunk.js"
+  },
+  {
+    "revision": "4f7eb5f0c39dbabe4d15",
+    "url": "/static/js/runtime~main.4f7eb5f0.js"
+  },
+  {
+    "revision": "afe2806c5e5c3c215fd4",
+    "url": "/static/js/4.afe2806c.chunk.js"
+  },
+  {
+    "revision": "d83b82d7c3a31ec3f323",
+    "url": "/static/js/5.d83b82d7.chunk.js"
+  },
+  {
+    "revision": "d200a5e6e4edbf8db02e",
+    "url": "/static/css/main.945bf447.chunk.css"
+  },
+  {
+    "revision": "d83b82d7c3a31ec3f323",
+    "url": "/static/css/5.bb8f3b1a.chunk.css"
+  },
+  {
+    "revision": "f3fb08a66fbce0a4b5ec38c1708789dd",
+    "url": "/index.html"
+  }
+];
